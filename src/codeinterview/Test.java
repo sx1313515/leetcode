20539c9552cf289e8ba23c4ee89994b9e3143bb5
song <sx1313515@gmail.com>
2021-03-31 07:17:29 +0800

    Merge branch 'master' of https://github.com/sx1313515/leetcode
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- src/codeinterview/Test.java
+++ src/codeinterview/Test.java
@@ -3,8 +3,28 @@ import java.util.*;
 
 public class Test{
     public static void main(String[] args) {
+        PriorityQueue queue = new PriorityQueue();
+        queue.add(2);
+        queue.add(3);
+        queue.add(1);
+        System.out.println(queue);
     }
     static class stu{
         public int age;
+    }
+    public int[] getLeastNumbers(int[] arr, int k) {
+        PriorityQueue queue = new PriorityQueue();
+        for(int a : arr) {
+            if(queue.size() == k) {
+                queue.poll();
+            }
+            queue.add(a);
+        }
+        return null;
+//        return queue.toArray(new int[2]);
+    }
+    public Integer[] getLeastNumbers1(int[] arr, int k) {
+        ArrayList<Integer> queue = new ArrayList();
+        return queue.toArray(new Integer[2]);
     }
 }
--- src/realinterviewquestions/jianzhioffer/Offer38.java
+++ src/realinterviewquestions/jianzhioffer/Offer38.java
@@ -3,7 +3,7 @@ package realinterviewquestions.jianzhioffer;
 import java.util.HashSet;
 
 /**
- * 描述:
+ * 描述:排列
  *
  * @Author: xiang_song
  * @CreateDate: 2021/3/30 10:16 下午
@@ -32,10 +32,10 @@ public class Offer38 {
             result.add(sb.toString());
             return; //推出进入到回溯
         }
-        for (int j = 0; j < length; j++) { //取每一个树都会全部遍历一遍，通过userd数组来记录，这样就不用remove和add了
+        for (int j = 0; j < length; j++) { //取每一个数都会全部遍历一遍，通过userd数组来记录，这样就不用remove和add了
             if (!userd[j]) { //这个元素没用过
                 userd[j] = true;
-                sb.append(chars[i]);
+                sb.append(chars[j]);
                 //递归
                 dfs(chars,i+1,sb,length);  //i+1是表示当前字符串的长度追加1，sb得传进去
                 //回溯
